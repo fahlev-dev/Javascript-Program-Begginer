7a41a52e9aa1002134a270f6e81b690a66768ffc
Reza Fahlevi <58735418+fahlev-dev@users.noreply.github.com>
2026-05-01 23:21:43 +0700

    Add files via upload
    first commit

---
+++ Student Grade Calculator/index.js
@@ -0,0 +1,19 @@
+function throttle(fn, delay) {
+    let lastTime = 0;
+    return function(...args) {
+        let now = Date.now();
+        if (now-lastime >= delay) {
+            fn.apply(this, args);
+            lastTime = now;
+        }
+    };
+}
+
+function printMessage() {
+    console.log("function executed at", new Date().toLocaleTimeString());
+}
+
+const throttledPrint = throttle(printMessage, 1000);
+
+setInterval(throttledPrint, 200);
+
